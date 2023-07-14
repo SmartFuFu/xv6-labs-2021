b039d4f2865a6e673334d03aff9533b3c635b483
SmartFuFu <1748189285@qq.com>
2023-07-14 15:37:20 +0800

    wrote a sleep file

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc,char*argv[])
+{
+    if(argc==1){
+        printf("Please enter the parameters!\n");
+    }
+    else{
+        int duration =atoi(argv[1]);
+        sleep(duration);
+    }
+    exit(0);
+}
